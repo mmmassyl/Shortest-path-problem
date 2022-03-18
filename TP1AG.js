ebc7e7f91a730f21884e5769aac12daed14ab864
MassylHaddad <massyl.haddad@gmail.com>
2022-03-18 17:23:25 +0100

    TP1

---
+++ TP1AG.js
@@ -0,0 +1,37 @@
+class Graphe {
+    arc(graphe, sommetX, sommetY) {
+
+    }
+
+    arete(graphe, sommetX, sommetY) {
+
+    }
+
+    saisie(graphe) {
+
+    }
+
+    succ(graphe, sommetX) {
+
+    }
+
+    pred(graphe, sommetX) {
+
+    }
+
+    desc(graphe, sommetX) {
+
+    }
+
+    anc(graphe, sommetX) {
+
+    }
+
+    compCon(graphe, sommetX) {
+
+    }
+
+    nbCompCon(graphe, sommetX) {
+
+    }
+}
